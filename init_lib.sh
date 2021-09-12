2a4255bc5365c4c5771242a172ce1e2cd74b9669
Andrea-Favullo <favullo.andrea.dennis@gmail.com>
2021-09-12 22:19:25 +0000

    initial commit

---
+++ init_lib.sh
@@ -0,0 +1,20 @@
+cd /workspace/PersonalWebSite ;
+    echo -e "\n-----------------------------\n\nInstallazione Angular CLI ..." ;
+    npm i -g @angular/cli ;
+    echo -e "\n-----------------------------\n\nInstallazione Nodemon ..." ;
+    npm i -g nodemon ;
+cd client/ ;
+    echo -e "\n-----------------------------\n\n[CLIENT] Installazione CORS ..." ;
+    npm i cors ;
+    echo -e "\n-----------------------------\n\n[CLIENT] Installazione Dipendenze ..." ;
+    npm i ;
+cd ../server ;
+    echo -e "\n-----------------------------\n\n[SERVER] Installazione CORS ..." ;
+    npm i cors ;
+    echo -e "\n-----------------------------\n\n[SERVER] Installazione Redis ..." ;
+    npm i redis ;
+    echo -e "\n-----------------------------\n\n[SERVER] Installazione Express ..." ;
+    npm i express ;
+    echo -e "\n-----------------------------\n\n[SERVER] Installazione Dipendenze ..." ;
+    npm i ;
+echo -e "[FINITO]"
